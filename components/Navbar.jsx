476087a331509072a1360a1508159d59add629f8
Phuc Le (Gin) <hoangphuc2832000@gmail.com>
2022-12-05 21:44:36 +1100

    Adding hero section

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => (
       <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img 
       src="/search.svg" alt="search" className='w-[24px] h-[24px] object-contain' />
-      <h2 className='font-mono text-[24px] leading-[30px] text-white'>METAVERSUS</h2>
+      <h2 className='font-mono text-[24px] leading-[30px] text-white'>METAVERSE</h2>
       <img src="/menu.svg" alt="menu" className='w-[24px] h-[24px] object-contain' />
     </div>
   </motion.nav>
