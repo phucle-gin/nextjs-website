d67cc7babe9484d79877b41c9e50177cba9fbe64
Phuc Le (Gin) <hoangphuc2832000@gmail.com>
2023-01-02 23:38:26 +1100

    Adding Insight section with styles

--- constants/index.js
+++ constants/index.js
@@ -52,13 +52,13 @@ export const insights = [
     imgUrl: '/planet-06.png',
     title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
     subtitle:
-        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie.',
   },
   {
     imgUrl: '/planet-07.png',
     title: '7 tips to easily master the madness of the Metaverse',
     subtitle:
-        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus.',
   },
   {
     imgUrl: '/planet-08.png',
