08f7179c196858c5f1e26ee9aea60b9a71d20ffe
Phuc Le (Gin) <hoangphuc2832000@gmail.com>
2023-04-13 20:20:48 +1000

    Finalizing

--- app/page.js
+++ app/page.js
@@ -1,37 +1,10 @@
 'use client';
 import dynamic from 'next/dynamic'
-// import { Footer, Navbar } from '../components';
-// import { Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+import { Footer, Navbar } from '../components';
+import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew } from '../sections';
 import { LazyMotion, domAnimation } from "framer-motion"
 import { Suspense } from 'react';
-const About = dynamic(() => import('../sections/About'), {
-  suspense: true,
-})
-const Hero = dynamic(() => import('../sections/Hero'), {
-  suspense: true,
-})
-const Explore = dynamic(() => import('../sections/Explore'), {
-  suspense: true,
-})
-const Feedback = dynamic(() => import('../sections/Feedback'), {
-  suspense: true,
-})
-const GetStarted = dynamic(() => import('../sections/GetStarted'), {
-  suspense: true,
-})
-const Insights = dynamic(() => import('../sections/Insights'), {
-  suspense: true,
-})
-const WhatsNew = dynamic(() => import('../sections/WhatsNew'), {
-  suspense: true,
-})
 
-const Footer = dynamic(() => import('../components/Footer'), {
-  suspense: true,
-})
-const Navbar = dynamic(() => import('../components/Navbar'), {
-  suspense: true,
-})
 const Page = () => (
   <LazyMotion features={domAnimation}>
     <Suspense>
