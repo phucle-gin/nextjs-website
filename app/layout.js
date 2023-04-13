4acd3b707a94b67ba06a32bbecf33dce026bbae8
Phuc Le (Gin) <hoangphuc2832000@gmail.com>
2023-04-13 20:42:04 +1000

    Finalizing

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ const RootLayout = ({ children }) => (
   <html lang="en">
     <head>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cute+Font&family=Space+Mono&display=swap">
       </link>    
     </head>
